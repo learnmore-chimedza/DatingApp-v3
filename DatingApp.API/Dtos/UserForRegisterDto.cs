a9366c97673d5a46a790daa874def6b8672d56b5
chimedzalearnmore@gmail.com <chimedzalearnmore@gmail.com>
2019-11-13 14:44:20 +0200

    End of Section 10

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,14 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace DatingApp.API.Dtos
+﻿namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-        [Required]
         public string Username { get; set; }
-
-        [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
