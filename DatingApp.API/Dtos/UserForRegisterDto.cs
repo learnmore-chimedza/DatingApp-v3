7e6691dfb1453a4e0a7c2c66cd360ad71e682945
chimedzalearnmore@gmail.com <chimedzalearnmore@gmail.com>
2019-11-18 12:47:52 +0200

    End of Section 12

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,36 @@
-﻿namespace DatingApp.API.Dtos
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+       
+        [Required]
         public string Password { get; set; }
+       
+        [Required]
+        public string Gender { get; set; }
+        
+        [Required]
+        public string KnownAs { get; set; }
+        
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
